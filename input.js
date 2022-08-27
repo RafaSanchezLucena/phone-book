b9c262194008b492840f6d9a8c285d1de9753d09
Rafa Sánchez Lucena <sanchez.lucena@gmail.com>
2022-08-27 18:36:48 +0200

    primer commit

---
+++ input.js
@@ -0,0 +1,22 @@
+export const phoneBook = [
+  {
+    nombre: "Informatica General",
+    numero: 777888,
+  },
+  {
+    nombre: "Informatica 1",
+    numero: 111111,
+  },
+  {
+    nombre: "Informatica 2",
+    numero: 555555,
+  },
+  {
+    nombre: "Secretaria",
+    numero: 666666,
+  },
+  {
+    nombre: "Admision",
+    numero: 777777,
+  }
+];
---
+++ main.js
@@ -0,0 +1,31 @@
+import "./style.css";
+import { phoneBook } from "./input";
+
+document.querySelector("#app").innerHTML = /*html*/ `<div>
+    <h1>Guía telefónica</h1>
+    <input type="text" onkeyup="searchNumber(value)" class="form-control" autofocus placeholder="Introduce el criterio de búsqueda..."/> 
+    <h4>Resultado de la búsqueda:</h4>
+    <ul class="lista"></ul>
+    <footer><a href="">Contacta@</a></footer>
+
+  </div>
+`;
+
+// Función encargada de buscar coincidencias según el valor introducido en el input.
+window.searchNumber = (value) => {
+  if (value != "") {
+    const phoneBookFilter = phoneBook.filter((element) =>
+      element.nombre.toLowerCase().includes(value.toLowerCase())
+    );
+
+    const dataPhone = phoneBookFilter.map(
+      (element) =>
+        `<li>${element.nombre}: <strong>${element.numero}</strong> </li>`
+    );
+    document.querySelector(".lista").innerHTML = dataPhone.join(" ");
+  } else {
+    document.querySelector(
+      ".lista"
+    ).innerHTML = /*html*/ `<h5>Sin resultados...</h5>`;
+  }
+};
